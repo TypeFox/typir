f932abf86211adca362470d574cad867923900ae
Markus Rudolph <markhoernchen@gmx.de>
2024-06-17 10:49:03 +0200

    Add Lox as example (#6)
    * Add Lox
    
    * Add Typir instance for Lox (unfinished)

---
+++ examples/lox/src/lox-scope.ts
@@ -0,0 +1,48 @@
+/******************************************************************************
+ * Copyright 2024 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
+import { DefaultScopeProvider, EMPTY_SCOPE, AstUtils, ReferenceInfo, Scope, LangiumCoreServices } from "langium";
+import { Class, isClass, MemberCall } from "./generated/ast.js";
+import { isClassType } from "./type-system/descriptions.js";
+import { getClassChain, inferType } from "./type-system/infer.js";
+
+export class LoxScopeProvider extends DefaultScopeProvider {
+
+    constructor(services: LangiumCoreServices) {
+        super(services);
+    }
+
+    override getScope(context: ReferenceInfo): Scope {
+        // target element of member calls
+        if (context.property === 'element') {
+            // for now, `this` and `super` simply target the container class type
+            if (context.reference.$refText === 'this' || context.reference.$refText === 'super') {
+                const classItem = AstUtils.getContainerOfType(context.container, isClass);
+                if (classItem) {
+                    return this.scopeClassMembers(classItem);
+                } else {
+                    return EMPTY_SCOPE;
+                }
+            }
+            const memberCall = context.container as MemberCall;
+            const previous = memberCall.previous;
+            if (!previous) {
+                return super.getScope(context);
+            }
+            const previousType = inferType(previous, new Map());
+            if (isClassType(previousType)) {
+                return this.scopeClassMembers(previousType.literal);
+            }
+            return EMPTY_SCOPE;
+        }
+        return super.getScope(context);
+    }
+
+    private scopeClassMembers(classItem: Class): Scope {
+        const allMembers = getClassChain(classItem).flatMap(e => e.members);
+        return this.createScopeForNodes(allMembers);
+    }
+}
---
+++ examples/lox/src/type-system/assignment.ts
@@ -0,0 +1,47 @@
+/******************************************************************************
+ * Copyright 2024 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+import { isClassType, isFunctionType, isNilType, TypeDescription } from "./descriptions.js";
+import { getClassChain } from "./infer.js";
+
+export function isAssignable(from: TypeDescription, to: TypeDescription): boolean {
+    if (isClassType(from)) {
+        if (!isClassType(to)) {
+            return false;
+        }
+        const fromLit = from.literal;
+        const fromChain = getClassChain(fromLit);
+        const toClass = to.literal;
+        for (const fromClass of fromChain) {
+            if (fromClass === toClass) {
+                return true;
+            }
+        }
+        return false;
+    }
+    if (isNilType(from)) {
+        return isClassType(to);
+    }
+    if (isFunctionType(from)) {
+        if (!isFunctionType(to)) {
+            return false;
+        }
+        if (!isAssignable(from.returnType, to.returnType)) {
+            return false;
+        }
+        if (from.parameters.length !== to.parameters.length) {
+            return false;
+        }
+        for (let i = 0; i < from.parameters.length; i++) {
+            const fromParam = from.parameters[i];
+            const toParam = to.parameters[i];
+            if (!isAssignable(fromParam.type, toParam.type)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return from.$type === to.$type;
+}
