a945fd52ba23034c50b3e3e32b2ee38f6c8b9e25
Markus Rudolph <markus.rudolph@typefox.io>
2025-02-04 09:22:06 +0100

    Add type system file

---
+++ examples/expression/src/type-system.ts
@@ -0,0 +1,6 @@
+/******************************************************************************
+ * Copyright 2024 TypeFox GmbH
+ * This program and the accompanying materials are made available under the
+ * terms of the MIT License, which is available in the project root.
+ ******************************************************************************/
+
